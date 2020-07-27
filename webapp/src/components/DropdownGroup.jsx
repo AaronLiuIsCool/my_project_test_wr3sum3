7fc00b23ae0e681e562946eb37b4a871280e022d
Q Sang <600722+blaxkmaze@users.noreply.github.com>
2020-07-26 20:33:38 -0600

    TF-0000: make dropdown component to be stateless (#29)
    * TF-0000: update tests and some minor fixes
    
    * chore: update snapshot
    
    Co-authored-by: Q Sang <Qitao_Sang@intuit.com>

--- webapp/src/components/DropdownGroup.jsx
+++ webapp/src/components/DropdownGroup.jsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Form } from 'react-bootstrap';
 import { Typeahead } from 'react-bootstrap-typeahead';
 
 import 'react-bootstrap-typeahead/css/Typeahead.css';
 
-// Searchable dropdown component
-const DropdownGroup = ({ label, id, placeholder, searchKey, options }) => {
-	const [singleSelections, setSingleSelections] = useState([]);
-  return (
-		<Form.Group className="form_item">
-			<Form.Label htmlFor={id}>{label}</Form.Label>
-			<Typeahead id={id} labelKey={searchKey} onChange={setSingleSelections} options={options} placeholder={placeholder} selected={singleSelections} />
-		</Form.Group>
-  );
+// TODO: Update consumer to pass in select and handleChange
+const DropdownGroup = ({ label, id, placeholder, searchKey, options, select, handleChange }) => (
+	<Form.Group className="form_item">
+		<Form.Label htmlFor={id}>{label}</Form.Label>
+		<Typeahead id={id} labelKey={searchKey} options={options}
+			placeholder={placeholder} selected={select}
+			onChange={handleChange} />
+	</Form.Group>
+);
+
+
+DropdownGroup.propTypes = {
+	label: PropTypes.string,
+	id: PropTypes.string,
+	placeholder: PropTypes.string,
+	searchKey: PropTypes.string,
+	options: PropTypes.array,
+	select: PropTypes.string,
+	handleChange: PropTypes.func
 };
 
 export default DropdownGroup;
