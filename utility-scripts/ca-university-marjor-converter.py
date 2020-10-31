6ebf6adf982ed6c817b02f8b46fd58191b0d97ca
Oscar Zhang <zsyoscar@gmail.com>
2020-10-31 08:19:46 -0700

    update major dropdown (#277)
    * update major dropdown
    
    * update character encoding
    
    Co-authored-by: Q Sang <600722+blaxkmaze@users.noreply.github.com>

--- utility-scripts/ca-university-marjor-converter.py
+++ utility-scripts/ca-university-marjor-converter.py
@@ -10,13 +10,13 @@ reader = csv.DictReader( inputCSVFile, fieldnames)
 rows = list(reader)
 totalrows = len(rows)
 
-with open(outputFileName, 'w', encoding='utf8') as outputJSONFile:
+with open(outputFileName, 'w', encoding='utf-8-sig') as outputJSONFile:
   outputJSONFile.write('[\n')
   for index, row in enumerate(rows):
     if index != totalrows-1:
-      outputJSONFile.write('{"id":"' + str(index+1) + '", "major": "' + row['m'] + '"},\n')
+      outputJSONFile.write('{"id":"' + str(index+1) + '", "data": "' + row['m'] + '"},\n')
     else:
-      outputJSONFile.write('{"id":"' + str(index+1) + '", "major": "' + row['m'] + '"}\n')
+      outputJSONFile.write('{"id":"' + str(index+1) + '", "data": "' + row['m'] + '"}\n')
   outputJSONFile.write(']')
 
 inputCSVFile.close()
