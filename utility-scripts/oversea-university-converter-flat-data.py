85465cce7624813394ab466dbff53916173d5dae
Oscar Zhang <zsyoscar@gmail.com>
2020-10-31 19:39:36 -0700

    add oversea universities data based on available countries (#292)
    Co-authored-by: Oscar Zhang <zsyoscar@gamil.com>

---
+++ utility-scripts/oversea-university-converter-flat-data.py
@@ -0,0 +1,31 @@
+import csv
+import json
+
+inputCSVFile = open('./data/ oversea-university.csv', 'r')
+outputFileName = './data/ oversea-university-flat.json'
+
+fieldnames = ["city", "country"]
+supported_country_list = ['au', 'ca', 'cn', 'in', 'jp', 'kr', 'uk', 'us']
+reader = csv.DictReader( inputCSVFile, fieldnames)
+
+rows = list(reader)
+totalrows = len(rows)
+
+with open(outputFileName, 'w', encoding='utf-8-sig') as outputJSONFile:
+  outputJSONFile.write('[\n')
+
+  for country in supported_country_list:
+    # outputJSONFile.write('"'+country+'":\n [')
+    for index, row in enumerate(rows):
+      
+      if (country != row['country']):
+        continue
+      
+      outputJSONFile.write('{"id":"' + str(index+1) + '", "data": "' + row['city'] + '"},\n')
+        
+    # outputJSONFile.write('],\n')
+
+    
+  outputJSONFile.write(']')
+
+inputCSVFile.close()
--- utility-scripts/province-city-converter.py
+++ utility-scripts/province-city-converter.py
@@ -16,7 +16,7 @@ rows2 = list(reader2)
 totalrows2 = len(rows2)
 
 
-with open(outputFileName, 'w', encoding='utf8') as outputJSONFile:
+with open(outputFileName, 'w', encoding='utf-8-sig') as outputJSONFile:
   outputJSONFile.write('[\n')
   for index, row in enumerate(rows):
     # Use the ensure_ascii=False to force to encode the value to UTF-8:
