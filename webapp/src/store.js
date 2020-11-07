ea3c80e6a3288895a9974a764ebbeb14d6e6d3e9
Q Sang <600722+blaxkmaze@users.noreply.github.com>
2020-11-07 09:00:51 -0700

    TF-269 Add job bookmarked detection logic (#304)
    TF-269 Add job bookmarked detection logic

--- webapp/src/store.js
+++ webapp/src/store.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import appReducer from 'features/App/slicer';
 import accountReducer from 'features/App/slicer/account';
 import resumeReducer from 'features/SmartResume/slicer';
+import jobsMatcherReducer from 'features/JobsMatcher/slicer';
 
 export default configureStore({
   reducer: {
     app: appReducer,
     account: accountReducer,
-    resume: resumeReducer
+    resume: resumeReducer,
+    jobs: jobsMatcherReducer,
   },
 });
