4be1b399ba2ccd706d43ea60fffb147746d8d3dc
Oscar Zhang <zsyoscar@gmail.com>
2020-11-10 20:42:02 -0800

    Adjust the way of how we passing file to resumeSDK (#295)
    * Adjust the way of how we passing file to resumeSDK
    
    * fix the bug on resume SDK
    
    * fix photo upload issue
    
    * fix photo upload issue
    
    * code update based on review
    
    * update a prop check to pass test
    
    * update config yaml file
    
    * update snapshot
    
    * updaet dockerfile
    
    Co-authored-by: Oscar Zhang <zsyoscar@gamil.com>
    Co-authored-by: tengr <tengrc1106@gmail.com>

--- webapp/src/features/SmartResume/components/SmartResume.test.js
+++ webapp/src/features/SmartResume/components/SmartResume.test.js
@@ -2,25 +2,40 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
-
+import { mount } from 'enzyme';
 import store from 'store';
 import SmartResume from './SmartResume';
 
 import 'react-dates/initialize';
 
 describe('SmartResume tests', () => {
-  // todo: avatar uploader has dynamic id, need to exclusive this 
-  test('renders Smart Resume', () => {
-  //   const smartResumeApp = renderer.create(
-  //     <Provider store={store}>
-  //       <BrowserRouter>
-  //         <SmartResume />
-  //       </BrowserRouter>
-  //     </Provider>
-  //   ).toJSON();
+  // Aavatar uploader has dynamic id attribute, need to exclusive this 
+  test.skip('renders Smart Resume', () => {
+    const smartResumeApp = renderer.create(
+      <Provider store={store}>
+        <BrowserRouter>
+          <SmartResume />
+        </BrowserRouter>
+      </Provider>
+    ).toJSON();
+    expect(smartResumeApp).toMatchSnapshot();
+  });
 
-  // expect({"smartResumeApp"}).toMatchSnapshot();
-    expect({}).toMatchSnapshot({});
+  test('renders Smart Resume by checking each components', () => {
+    const smartResumeApp = mount(
+      <Provider store={store}>
+        <BrowserRouter>
+          <SmartResume />
+        </BrowserRouter>
+      </Provider>
+    );
+    expect(smartResumeApp.find('.widgetContainer').length).toEqual(1);
+    expect(smartResumeApp.find('.progressBar.progress').length).toEqual(1);
+    expect(smartResumeApp.find('#basicInfo').length).toEqual(2);
+    expect(smartResumeApp.find('#education').length).toEqual(2);
+    expect(smartResumeApp.find('#workXp').length).toEqual(2);
+    expect(smartResumeApp.find('#otherXp').length).toEqual(2);
+    expect(smartResumeApp.find('#certifications').length).toEqual(2);
   });
 
 });
