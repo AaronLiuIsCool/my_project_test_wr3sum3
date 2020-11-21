01c4e6b29821063626a6491948d5c01bed04bd91
Joseph Jin <jerkjoe61@gmail.com>
2020-11-20 23:27:26 -0800

    fix duplicate after pull

--- webapp/src/shell/services/AccountServices.js
+++ webapp/src/shell/services/AccountServices.js
@@ -46,12 +46,4 @@ export default class AccountServices extends BaseServices {
       logger.error(err);
     }
   }
-  
-  async updateResume(resumeId, resumeDto,) {
-    try {
-      return await this.put(`/v1/account/resumes/${resumeId}`, resumeDto);
-    } catch (err) {
-      logger.error(err);
-    }
-  }
 }
