d776aae12b103b3398e66854d2394808eca94101
Spencer Li <bambamkuka@gmail.com>
2020-12-20 12:07:12 -0800

    [BE][TF-265] populate initial English suggestion data
    also to include a parser for generating SQL migration script
    from the excel file

---
+++ job-svc/src/main/resources/db/migration/V2__populate_suggestions_en_v1.sql
@@ -0,0 +1,283 @@
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Banking', 
+                'Commercial Banking', 
+                'Attended various marketing and networking events to originate prospective clients', 
+                'networking events,prospective clients'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Banking', 
+                'Commercial Banking', 
+                'Corrected systemic compliance issues, created system to handle on the front end, ensure staff compliance.', 
+                'compliance issue'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Banking', 
+                'Commercial Banking', 
+                'Managed $40,000,000 loan portfolio consisting of real estate, working capital and line of credit loans.', 
+                'loan portfolio,real estate,working capital,line of credit loan'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Banking', 
+                'Commercial Banking', 
+                'Implemented new procedures that improved documentation exceptions from 70% to 25%.', 
+                '25.0%,documentation exceptions,70.0%'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Banking', 
+                'Commercial Banking', 
+                'Managed commercial lending relationships with over 100 unique entities, business owners and chief financial officers.', 
+                'commercial lending relationships,100,unique entities,business owners,chief financial officers'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Banking', 
+                'Credit Analyst', 
+                'Managed credit limit database by setting, removing, and adjusting credit limits determined by strength of credit and ratio of credit limit used.', 
+                'credit limit database,strength of credit,ratio of credit limit'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Banking', 
+                'Credit Analyst', 
+                'Actively used Bradstreet and Equifax business reports to determine payment terms.', 
+                'Equifax,Bradstreet'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Banking', 
+                'Credit Analyst', 
+                'Resolved fraudulent activity matters by effective and efficient communication with all channels involved; including clients, vendors, and the finance and accounting department.', 
+                'effective and efficient communication'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Banking', 
+                'Credit Analyst', 
+                'Published a monthly newsletter to all employees informing them of recent fraud and credit related news.', 
+                'a monthly newsletter,fraud news,credit related news'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Banking', 
+                'Credit Analyst', 
+                'Prepared annual and semi-annual reports for management, justifying credit lines exceeding $350K', 
+                'annual ,semi-annual reports, $350K'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Banking', 
+                'Credit Analyst', 
+                'Analyzed severity of past due accounts and took precautions to prevent future credit losses', 
+                'severity,precautions'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Engineering', 
+                'Civil Eng.', 
+                'Collaborated with a team of 4 members, including project manager, project coordinator, technical engineer and structural engineer to design and construct a grain processing plant with additional capacity (worth CAD$ 1.5 million) for SK Grain Inc', 
+                '1.5 million,4,project manager'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Engineering', 
+                'Civil Eng.', 
+                'Used the finite element analysis (FEA) and MATLAB to minimize weights of the structure and ABAQUS to analyze the displacements of each node', 
+                'FEA,MATLAB,ABAQUS'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Engineering', 
+                'Civil Eng.', 
+                'Generated more than ten sets of 2D and 3D engineering drawings with AutoCAD and SAP2000', 
+                'AutoCAD,SAP2000,engineering drawings'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Engineering', 
+                'Civil Eng.', 
+                'Scheduled and participated in the bi-weekly meeting with clients and colleagues to ensure regular and efficient internal communication', 
+                'bi-weekly meeting,internal communication'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Engineering', 
+                'Civil Eng.', 
+                'Identified the business development opportunities and contributed to the development of technical and administrative support by managing the implementation and deployment of recent data into existing IP operation system', 
+                'business development opportunities,operation system'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Engineering', 
+                'Civil Eng.', 
+                'Summarized graphical data and wrote a final report to demonstrate the effectiveness of the design and rated as a top 10% project among all participants', 
+                '10.0%,graphical data'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Engineering', 
+                'Project Eng.', 
+                'Acted as liaison between client, engineering consultant, general contractor, and multi-disciplinary construction team and successfully completed projects with total revenue over 200 million', 
+                'liaison,200 million,general contractor,multi-disciplinary construction team'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Engineering', 
+                'Project Eng.', 
+                'Generated requests for design changes, clarifications, and technical/engineering issues with the clients/subcontractors by preparing RFI (request for information) and NCR (non-conformance report)', 
+                'RFI,NCR,non-conformance report,request for information'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Engineering', 
+                'Project Eng.', 
+                'Initiated change management process issuing Change Requests (CRQ) and preparing cost estimates for any scope changes', 
+                'Change Requests,CRQ,cost estimates'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Engineering', 
+                'Project Eng.', 
+                'Involved in project closeout including archiving documents, preparing turnover packages, and red-lining shop drawings to reflect design modifications', 
+                'project closeout,turnover packages,red-lining shop drawings'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Engineering', 
+                'Project Eng.', 
+                'Conducted quantity take-off across all disciplines including structural steel, electrical, piping, etc.', 
+                'quantity take-off'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Engineering', 
+                'Project Eng.', 
+                'Participated in the change management process, issuing Change Requests (CRQ) and preparing cost estimates for any scope changes', 
+                'change management process,Change Requests'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Engineering', 
+                'Project Eng.', 
+                'Provided technical supports to the project team by communicating with the consultants through Request for Information (RFIs) and Engineering Judgement (EJs)', 
+                'Engineering Judgement,Ejs,Request for Information,RFIs'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Lab tech', 
+                'Lab tech-chemical', 
+                'Characterized MnO-dotted dye-ligated silica nanoparticles using PXRD and SQUID', 
+                'PXRD,SQUID'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Lab tech', 
+                'Lab tech-chemical', 
+                'Developed an improved method for sample and buffer preparation, effectively minimizing fluorescein’s self-quenching effect.', 
+                'buffer preparation,self-quenching effect'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Lab tech', 
+                'Lab tech-chemical', 
+                'Successfully synthesized MnO-dotted dye-ligated silica nanoparticles with potential application in medical imaging', 
+                'medical imaging'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Lab tech', 
+                'Lab tech-chemical', 
+                'Prepared and ran 150 digested samples for Total Kjeldahl Nitrogen (TKN) daily', 
+                '150,TKN'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'IT & Data', 
+                'Data Scientist', 
+                'Worked in a team of 4 and followed Scrum and Agile framework to deliver the project values to the local clients', 
+                '4,Agile framework,Scrum'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'IT & Data', 
+                'Data Scientist', 
+                'Built regression and classification models with sklearn and keras packages to solve image-based problems', 
+                'sklearn,keras,classification models'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'IT & Data', 
+                'Data Scientist', 
+                'Developed a final model by combining CNN regression, sklearn regression, and classification', 
+                'CNN regression,sklearn regression'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'IT & Data', 
+                'Data Scientist', 
+                'Assisted in setting up docker-based environment on GPU-enabled server', 
+                'GPU-enabled server,docker-based environment'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'IT & Data', 
+                'Data Scientist', 
+                'Prepared and delivered presentations to the client in project sprint meetings', 
+                'sprint meetings'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'IT & Data', 
+                'Data Scientist', 
+                'Achieved project outcomes which are beyond the expectation of the clients and project goals', 
+                'project outcomes,beyond the expectation'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Projects', 
+                'Civil Eng.', 
+                'Collaborated with a team of 5 to estimate the cost of a municipal road project. (Cost: $1,961,186)', 
+                '5,1961186'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Projects', 
+                'Civil Eng.', 
+                'Worked with a team of 6 to design a 2-story building in the campus for studying and entertainment.', 
+                '2-story building'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Projects', 
+                'Civil Eng.', 
+                'Developed deep understanding of building construction standards, such as LEED Canada NC 2009 and LEED Canada CS 2009 Rating System, and National Building Code of Canada', 
+                'LEED,Rating System'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Projects', 
+                'Civil Eng.', 
+                'Designed a gray water recycling system following the LEED Silver design criteria.', 
+                'LEED Silver'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Projects', 
+                'Civil Eng.', 
+                'Drafted two 3D model drawings of the house and surroundings using Revit 2018', 
+                'Revit 2018'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Projects', 
+                'Civil Eng.', 
+                'Applied surveying equipment including GNSS systems, total stations and locators to measure the elevations, distances and angles', 
+                'GNSS systems,total stations'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Projects', 
+                'Computer Sci.', 
+                'Implemented reinforcement learning algorithms like value iteration/policy iteration/SARSA/Q-learning/Monte-Carlo/Temporal Difference with Python in a grid world scenario to test each algorithm’s strength and weakness', 
+                'value iteration,Q-learning,Monte-Carlo,Temporal Difference'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Projects', 
+                'Computer Sci.', 
+                'Analyzed and designed a mobile app using Android Studio and Java as a team leader of 5 students over 3 months.', 
+                'Android Studio,Java,5 students,3 months'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Projects', 
+                'Computer Sci.', 
+                'Followed SDLC and MVC structure and used elastic search as backend REST API.', 
+                'SDLC,MVC structure ,REST API'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Projects', 
+                'Computer Sci.', 
+                'Led the team from scratch design to release to professor with high Junit tests coverage by applying Agile and Scrum.', 
+                'scratch design,high Junit tests coverage,Agile,Scrum'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Projects', 
+                'Computer Sci.', 
+                'Created new database applications based on Python, C, SQLite and MySQL, focusing on query processing, query
+optimization, database normalization, and database stability.', 
+                'Python,SQLite,MySQL,query processing'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Projects', 
+                'Computer Sci.', 
+                'Focused on improving user experiences and the final score of this project is 25% higher than the average.', 
+                'user experiences,25.0%'
+            );
+INSERT INTO `suggestion` (`industry`, `position_title`, `texts`, `suggestion_keywords`) VALUES (
+                'Projects', 
+                'Computer Sci.', 
+                'Focused on analyze and design phrases of the SDLC and created UML diagram, such as Class diagram and Sequence Diagram', 
+                'SDLC,UML diagram,Class diagram,Sequence Diagram'
+            );
